db19b95666471fb4d69382ee863d255d5f834a13
Lavrenko Anastasia <lavrenko.a@gmail.com>
2017-04-03 14:09:16 +0300

    fix comments

--- main.py
+++ main.py
@@ -17,12 +17,13 @@ redmine_host = config['redmine']['api_host']
 redmine_api_key = config['redmine']['api_key']
 project_name = config['redmine']['project']
 
-# Load all urls from google file (TODO check color status - api cannot do this?)
+
 sheet_id = config['google']['sheet_id']
 sheet_range = config['google']['sheet_range']
 
-# Transform links to ids
+# Load all urls from google file (TODO check color status - api cannot do this?)
 all_links = load_links_from_google(sheet_id, sheet_range)
+# Transform links to ids
 all_ids = sorted(map(lambda x: get_comment_ids(x), all_links))
 
 # Connect to stepik
@@ -37,9 +38,20 @@ if not token:
 # Connect to redmine
 redmine_server = Redmine(redmine_host, key=redmine_api_key)
 
-project = redmine_server.project.get(project_name)
+
+def create_new_task(comment):
+    issue = redmine_server.issue.new()
+    issue.project_id = project_name
+    issue.subject = 'User {} comment {}'.format(comment.user, current_id)
+    issue.description = comment.text
+    issue.custom_fields = [{'id': 16, 'value': current_id}, {'id': 15, 'value': comment.user.id}]
+    issue.save()
+
+    return issue
+
 
 for parent_id, current_id in all_ids:
+    # Try to load root and current comments
     parent = get_comment(stepik_api_host, token, parent_id)
     current = get_comment(stepik_api_host, token, current_id)
 
@@ -51,20 +63,17 @@ for parent_id, current_id in all_ids:
         print("skip", current_id)
         continue
 
+    # search existing in redmine
     parent_task = redmine_server.issue.filter(project_id=project_name, cf_16=parent_id)
     parent_task_id = parent_task[0].id if parent_task else None
 
     current_task = redmine_server.issue.filter(project_id=project_name, cf_16=current_id)
     if current_task:
-        print("skip", current_id, current_task.resources)
+        print("Already created for:", current_id)
         continue
 
-    sub_issue = redmine_server.issue.new()
-    sub_issue.project_id = project_name
-    sub_issue.subject = 'User {} comment {}'.format(current.user, current_id)
-    sub_issue.description = current.text
-    sub_issue.custom_fields = [{'id': 16, 'value': current_id}, {'id': 15, 'value': current.user.id}]
-    sub_issue.save()
+    # Create new task if not found
+    sub_issue = create_new_task(current)
 
     if parent_task_id:
         redmine_server.issue_relation.create(issue_id=sub_issue.id, issue_to_id=parent_task_id,
